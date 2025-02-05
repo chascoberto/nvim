ffb1306474a90d9be143ae4215698e707cb41e84
chascoberto <rarteaga.it@gmail.com>
2025-02-05 10:17:26 -0300

    add config for lazy and ops nvim

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("vim-ops")
+
+require("config.lazy")
---
+++ lua/vim-ops.lua
@@ -0,0 +1,12 @@
+vim.cmd("set mouse=a") --use mouse
+vim.cmd("set relativenumber") --show numbers
+vim.cmd("syntax enable") --highlighting
+vim.cmd("set showcmd")
+vim.cmd("set showmatch") -- ()
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=0")
+vim.cmd("set softtabstop=0")
+vim.cmd("set autoindent")
+vim.cmd("set smarttab")
+
